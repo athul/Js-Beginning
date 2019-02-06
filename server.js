e28ca67c918e0f3bb0b00d2059918b641044345b
Athul CA <athul8720@gmail.com>
2019-02-06 23:47:10 +0530

    upd server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http= require("http");
-http.createServer(function(request,response){
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("It's Alive!!");
-    response.end();
+http.createServer(function(req,res){
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    res.write("It's ....Really Alive!!");
+    res.end();
 }).listen(3000);
