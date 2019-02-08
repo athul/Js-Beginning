96b5526ec9dc6e5f191e0640ab342b26920c85f8
Athul CA <athul8720@gmail.com>
2019-02-08 20:40:06 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http= require("http");
+var dt=require("./my_first_mod");
 http.createServer(function(req,res){
     res.writeHead(200,{"Content-Type":"text/plain"});
-    res.write("It's ....Really Alive!!");
+    res.write("It's...Really Alive!!\n");
+    res.write("The date and time are currently: " + dt.myDateTime());
     res.end();
 }).listen(3000);
