8c5d431380a5eacd4d6a938f4d48e3cf3bad3730
Athul CA <athul8720@gmail.com>
2019-02-08 20:39:13 +0530

    Revert "bleh"
    This reverts commit ebb98488c9f44aeb38279c8d2ac6e26d5075eb5f.

--- demo_http.js
+++
@@ -1,6 +0,0 @@
-var http=require("http");
-http.createServer(function(require,response){
-    response.writeHead(200,{'Content-Type':'text/head'});
-    response.write(require.url);
-    response.end();    
-}).listen(3000);
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var http= require("http");
-var dt=require("./my_first_mod");
 http.createServer(function(req,res){
     res.writeHead(200,{"Content-Type":"text/plain"});
-    res.write("It's...Really Alive!!\n");
-    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write("It's ....Really Alive!!");
     res.end();
 }).listen(3000);
